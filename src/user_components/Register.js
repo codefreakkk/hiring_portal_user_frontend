17961c698c6f395ba3c8cfe254b84f491d4e56cc
harsh said <harshsaid558@gmail.com>
2022-12-10 14:11:09 +0530

    code update 10-12-22

--- src/user_components/Register.js
+++ src/user_components/Register.js
@@ -1,14 +1,51 @@
-import React from "react";
+import { React, useState } from "react";
 import logo1 from "../user_assets/imgs/page/login-register/img-1.svg";
-import logo2 from "../user_assets/imgs/page/login-register/img-2.svg"
+import logo2 from "../user_assets/imgs/page/login-register/img-2.svg";
 import Header from "./Header";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  function handleSubmit() {
+    if (
+      fullName.length == 0 ||
+      email.length == 0 ||
+      password.length == 0 ||
+      rePassword.length == 0 ||
+      password != rePassword
+    ) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    axios
+      .post("http://localhost:8000/api/registeruser", {
+        userName: fullName,
+        userEmail: email,
+        userPassword: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status == true) {
+          alert("You have registered successfully");
+        } else if (response.data.status == false) {
+          alert("Some error occured. User not registered");
+        } else if (response.data.status == "email") {
+          alert("User already registered");
+        }
+      })
+      .catch((err) => alert("Some error occured. User not registered"));
+  }
+
   return (
     <>
-     <Header/>
+      <Header />
       <main class="main">
         <section class="pt-100 login-register">
           <div class="container">
@@ -26,9 +63,10 @@ function Register() {
                       class="form-control"
                       id="input-1"
                       type="text"
-                      required=""
+                      onChange={(e) => setFullName(e.target.value)}
+                      value={fullName}
                       name="fullname"
-                      placeholder="Steven Job"
+                      placeholder="Your name"
                     />
                   </div>
                   <div class="form-group">
@@ -38,10 +76,11 @@ function Register() {
                     <input
                       class="form-control"
                       id="input-2"
-                      type="email"
-                      required=""
+                      type="text"
+                      onChange={(e) => setEmail(e.target.value)}
+                      value={email}
                       name="emailaddress"
-                      placeholder="stevenjob@gmail.com"
+                      placeholder="example@gmail.com"
                     />
                   </div>
                   <div class="form-group">
@@ -52,7 +91,8 @@ function Register() {
                       class="form-control"
                       id="input-4"
                       type="password"
-                      required=""
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                       name="password"
                       placeholder="************"
                     />
@@ -65,7 +105,8 @@ function Register() {
                       class="form-control"
                       id="input-5"
                       type="password"
-                      required=""
+                      value={rePassword}
+                      onChange={(e) => setRePassword(e.target.value)}
                       name="re-password"
                       placeholder="************"
                     />
@@ -83,7 +124,8 @@ function Register() {
                   <div class="form-group">
                     <button
                       class="btn btn-brand-1 hover-up w-100"
-                      type="submit"
+                      type="button"
+                      onClick={handleSubmit}
                       name="login"
                     >
                       Submit &amp; Register
@@ -92,29 +134,22 @@ function Register() {
                   <div class="text-muted text-center">
                     Already have an account?{" "}
                     <NavLink to="/signup">
-                    <a>Sign in</a>
+                      <a>Sign in</a>
                     </NavLink>
                   </div>
                 </form>
               </div>
               <div class="img-1 d-none d-lg-block">
-                <img
-                  class="shape-1"
-                  src={logo1}
-                  alt="JobBox"
-                />
+                <img class="shape-1" src={logo1} alt="JobBox" />
               </div>
               <div class="img-2">
-                <img
-                  src={logo2}
-                  alt="JobBox"
-                />
+                <img src={logo2} alt="JobBox" />
               </div>
             </div>
           </div>
         </section>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
